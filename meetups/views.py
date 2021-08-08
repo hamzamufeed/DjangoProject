19f62c18373a3ec024b4c207c81d91e3a2827c39
Hamza Barabrah <HamzaBa@exalt.ps>
2021-08-08 13:00:58 +0300

    includes and inheritance

--- meetups/views.py
+++ meetups/views.py
@@ -13,7 +13,7 @@ def index(request):
         'meetups': meetups
     })
 
-def meetup_details(request):
+def meetup_details(request, meetup_slug):
     selected_meetup = {
         'title': 'A First Meetup', 'description': 'This is the first meetup!'
     }
